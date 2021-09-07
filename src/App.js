d4448b5710715c1f940e7dcb7ccf5f63643d7e19
Sean Sing <singzhenhau@gmail.com>
2021-09-07 14:14:05 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,119 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Auth, Hub } from "aws-amplify";
+
+const initialFormState = {
+  username: "",
+  password: "",
+  email: "",
+  authCode: "",
+  formType: "signUp",
+};
 
 function App() {
+  const [formState, updateFormState] = useState(initialFormState);
+  const [user, updateUser] = useState(null);
+
+  useEffect(() => {
+    checkUser();
+    setAuthListener();
+  }, []);
+  async function setAuthListener() {
+    Hub.listen("auth", (data) => {
+      switch (data.payload.event) {
+        case "signOut":
+          updateFormState(() => ({ ...formState, formType: "signUp" }));
+          break;
+        default:
+          break;
+      }
+    });
+  }
+  async function checkUser() {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      updateUser(user);
+      updateFormState(() => ({ ...formState, formType: "signedIn" }));
+    } catch (err) {
+      //updateUser(null)
+    }
+  }
+  function onChange(e) {
+    e.persist();
+    updateFormState(() => ({ ...formState, [e.target.name]: e.target.value }));
+  }
+  const { formType } = formState;
+
+  async function signUp() {
+    const { username, email, password } = formState;
+    await Auth.signUp({ username, password, attributes: { email } });
+    updateFormState(() => ({ ...formState, formType: "confirmSignUp" }));
+  }
+  async function confirmSignUp() {
+    const { username, authCode } = formState;
+    await Auth.confirmSignUp(username, authCode);
+    updateFormState(() => ({ ...formState, formType: "signIn" }));
+  }
+  async function signIn() {
+    const { username, password } = formState;
+    await Auth.signIn({ username, password });
+    updateFormState(() => ({ ...formState, formType: "signedIn" }));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {formType === "signUp" && (
+        <div>
+          <input name="username" onChange={onChange} placeholder="username" />
+          <input
+            name="password"
+            type="password"
+            onChange={onChange}
+            placeholder="password"
+          />
+          <input name="email" onChange={onChange} placeholder="email" />
+          <button onClick={signUp}>Sign Up</button>
+          <button
+            onClick={() =>
+              updateFormState(() => ({
+                ...formState,
+                formType: "signIn",
+              }))
+            }
+          >
+            Sign In
+          </button>
+        </div>
+      )}
+      {formType === "confirmSignUp" && (
+        <div>
+          <input
+            name="authCode"
+            onChange={onChange}
+            placeholder="confirmation code"
+          />
+          <button onClick={confirmSignUp}>Confirm Sign Up</button>
+        </div>
+      )}
+      {formType === "signIn" && (
+        <div>
+          <input name="username" onChange={onChange} placeholder="username" />
+          <input
+            name="password"
+            type="password"
+            onChange={onChange}
+            placeholder="password"
+          />
+          <button onClick={signIn}>Sign In</button>
+        </div>
+      )}
+      {formType === "signedIn" && (
+        <div>
+          <h1>Hello user!</h1>
+          <button onClick={() => Auth.signOut()}>Sign Out</button>
+        </div>
+      )}
     </div>
   );
 }
