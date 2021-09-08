2ab808430feb1a0de2e8e28fd2c956ba9e63d2ad
Sean Sing <singzhenhau@gmail.com>
2021-09-08 22:06:07 +0800

    updated styling for confirm, signup and welcome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 //import logo from "./logo.svg";
 import "./App.css";
-import SDLogo from "./assets/sd_full_logo.png";
+import SDLogo from "./assets/logo_snicker_darkbg.svg";
+import profile from "./assets/profile.svg";
 import { Auth, Hub } from "aws-amplify";
 import { Button, Input, Container } from "@chakra-ui/react";
 
@@ -11,6 +12,10 @@ const initialFormState = {
   email: "",
   authCode: "",
   formType: "signIn",
+  signInClicked: false,
+  signOutClicked: false,
+  signUpClicked: false,
+  confirmSignUpClicked: false,
 };
 
 function App() {
@@ -47,22 +52,38 @@ function App() {
     e.persist();
     updateFormState(() => ({ ...formState, [e.target.name]: e.target.value }));
   }
-  const { formType } = formState;
+  const { formType, signInClicked, signUpClicked, confirmSignUpClicked } =
+    formState;
 
   async function signUp() {
     const { username, email, password } = formState;
+    updateFormState(() => ({ ...formState, signUpClicked: "true" }));
     await Auth.signUp({ username, password, attributes: { email } });
-    updateFormState(() => ({ ...formState, formType: "confirmSignUp" }));
+    updateFormState(() => ({
+      ...formState,
+      formType: "confirmSignUp",
+      signUpClicked: false,
+    }));
   }
   async function confirmSignUp() {
     const { username, authCode } = formState;
+    updateFormState(() => ({ ...formState, confirmSignUpClicked: "true" }));
     await Auth.confirmSignUp(username, authCode);
-    updateFormState(() => ({ ...formState, formType: "signIn" }));
+    updateFormState(() => ({
+      ...formState,
+      formType: "signIn",
+      confirmSignUpClicked: false,
+    }));
   }
   async function signIn() {
     const { username, password } = formState;
+    updateFormState(() => ({ ...formState, signInClicked: "true" }));
     await Auth.signIn({ username, password });
-    updateFormState(() => ({ ...formState, formType: "signedIn" }));
+    updateFormState(() => ({
+      ...formState,
+      formType: "signedIn",
+      signInClicked: false,
+    }));
   }
 
   return (
@@ -71,81 +92,29 @@ function App() {
         maxW="sm"
         style={{
           textAlign: "left",
-          height: "60%",
-          paddingTop: "50px",
+          height: "100%",
+          padding: "50px",
+          paddingLeft: "45px",
         }}
       >
-        <img
-          src={SDLogo}
-          alt="Snickerdoodle Logo"
-          style={{ marginLeft: "auto", marginRight: "auto" }}
-        />
-        {formType === "signUp" && (
-          <div>
-            <div>
-              <label style={{ marginBottom: "5px" }}>Username</label>
-              <Input
-                name="username"
-                onChange={onChange}
-                placeholder="Enter your username"
-              />
-            </div>
-            <div>
-              <label style={{ marginBottom: "5px" }}>Password</label>
-              <Input
-                name="password"
-                type="password"
-                onChange={onChange}
-                placeholder="Enter your password"
-              />
-            </div>
-            <div>
-              <label>Email</label>
-              <Input
-                name="email"
-                onChange={onChange}
-                placeholder="Enter your email"
-              />
-            </div>
-            <Button colorScheme="blue" onClick={signUp}>
-              Button
-            </Button>
-
-            <Button
-              width=""
-              colorScheme="blue"
-              onClick={() =>
-                updateFormState(() => ({
-                  ...formState,
-                  formType: "signIn",
-                }))
-              }
-            >
-              Sign In
-            </Button>
-          </div>
-        )}
-        {formType === "confirmSignUp" && (
-          <div>
-            <Input
-              name="authCode"
-              onChange={onChange}
-              placeholder="confirmation code"
-            />
-            <button onClick={confirmSignUp}>Confirm Sign Up</button>
-          </div>
-        )}
         {formType === "signIn" && (
           <div
-            className="test"
             style={{
-              height: "100%",
               backgroundCOlor: "red",
               display: "flex",
               flexDirection: "column",
               justifyContent: "space-around",
             }}
           >
+            <img
+              src={SDLogo}
+              alt="Snickerdoodle Logo"
+              style={{
+                marginLeft: "auto",
+                marginRight: "auto",
+                display: "block",
+              }}
+            />
             <div style={{ paddingTop: "30px" }}>
               <h2 style={{ paddingBottom: "5px" }}>Username</h2>
               <Input
@@ -173,18 +142,36 @@ function App() {
                 }}
               />
             </div>
-            <Button
-              width="100%"
-              style={{
-                backgroundColor: "#232138",
-                borderRadius: "25px",
-                marginTop: "20px",
-                minHeight: "52px",
-              }}
-              onClick={signIn}
-            >
-              Sign In
-            </Button>
+            {signInClicked === false ? (
+              <Button
+                width="100%"
+                style={{
+                  backgroundColor: "#232138",
+                  borderRadius: "25px",
+                  marginTop: "20px",
+                  minHeight: "52px",
+                }}
+                onClick={signIn}
+                _focus={{ boxShadow: "none" }}
+              >
+                Sign In
+              </Button>
+            ) : (
+              <Button
+                isLoading
+                width="100%"
+                style={{
+                  backgroundColor: "#232138",
+                  borderRadius: "25px",
+                  marginTop: "20px",
+                  minHeight: "52px",
+                }}
+                onClick={signIn}
+                _focus={{ boxShadow: "none" }}
+              >
+                Sign In
+              </Button>
+            )}
 
             <h1 style={{ textAlign: "center", marginTop: "20px" }}>
               Don't have an account yet?{" "}
@@ -202,10 +189,214 @@ function App() {
             </h1>
           </div>
         )}
+        {formType === "signUp" && (
+          <div
+            style={{
+              backgroundCOlor: "red",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-around",
+            }}
+          >
+            <img
+              src={SDLogo}
+              alt="Snickerdoodle Logo"
+              style={{
+                marginLeft: "auto",
+                marginRight: "auto",
+                display: "block",
+              }}
+            />
+            <div style={{ paddingTop: "30px" }}>
+              <h2 style={{ paddingBottom: "5px" }}>Username</h2>
+              <Input
+                name="username"
+                onChange={onChange}
+                style={{
+                  backgroundColor: "white",
+                  border: "black",
+                  borderRadius: "6px",
+                  color: "black",
+                }}
+              />
+            </div>
+            <div style={{ marginTop: "10px" }}>
+              <h2 style={{ paddingBottom: "5px" }}>Password</h2>
+              <Input
+                name="password"
+                type="password"
+                onChange={onChange}
+                style={{
+                  backgroundColor: "white",
+                  border: "black",
+                  borderRadius: "6px",
+                  color: "black",
+                }}
+              />
+            </div>
+            <div style={{ marginTop: "10px" }}>
+              <h2 style={{ paddingBottom: "5px" }}>Email</h2>
+              <Input
+                name="email"
+                type="email"
+                onChange={onChange}
+                style={{
+                  backgroundColor: "white",
+                  border: "black",
+                  borderRadius: "6px",
+                  color: "black",
+                }}
+              />
+            </div>
+            {signUpClicked === false ? (
+              <Button
+                width="100%"
+                style={{
+                  backgroundColor: "#232138",
+                  borderRadius: "25px",
+                  marginTop: "20px",
+                  minHeight: "52px",
+                }}
+                onClick={signUp}
+                _focus={{ boxShadow: "none" }}
+              >
+                Create Account
+              </Button>
+            ) : (
+              <Button
+                isLoading
+                width="100%"
+                style={{
+                  backgroundColor: "#232138",
+                  borderRadius: "25px",
+                  marginTop: "20px",
+                  minHeight: "52px",
+                }}
+                onClick={signUp}
+                _focus={{ boxShadow: "none" }}
+              >
+                Create Account
+              </Button>
+            )}
+
+            <h1 style={{ textAlign: "center", marginTop: "20px" }}>
+              Already have an account?{" "}
+              <button
+                style={{ color: "#FFD68F" }}
+                onClick={() =>
+                  updateFormState(() => ({
+                    ...formState,
+                    formType: "signIn",
+                  }))
+                }
+              >
+                Sign In
+              </button>
+            </h1>
+          </div>
+        )}
+
+        {formType === "confirmSignUp" && (
+          <div
+            style={{
+              textAlign: "center",
+              marginTop: "80px",
+            }}
+          >
+            <h1 style={{ fontSize: "30px" }}>Confirmation</h1>
+            <h2 style={{ marginTop: "20px", marginBottom: "40px" }}>
+              Please confirm your account using the 6 digit code we sent to your
+              email address
+            </h2>
+            <Input
+              name="authCode"
+              onChange={onChange}
+              style={{
+                backgroundColor: "white",
+                border: "black",
+                borderRadius: "6px",
+                color: "black",
+              }}
+            />
+            {confirmSignUpClicked === false ? (
+              <Button
+                width="100%"
+                style={{
+                  backgroundColor: "#232138",
+                  borderRadius: "25px",
+                  marginTop: "80px",
+                  minHeight: "52px",
+                }}
+                _focus={{ boxShadow: "none" }}
+                onClick={confirmSignUp}
+              >
+                Confirm
+              </Button>
+            ) : (
+              <Button
+                isLoading
+                width="100%"
+                style={{
+                  backgroundColor: "#232138",
+                  borderRadius: "25px",
+                  marginTop: "20px",
+                  minHeight: "52px",
+                }}
+                onClick={confirmSignUp}
+                _focus={{ boxShadow: "none" }}
+              >
+                Confirm
+              </Button>
+            )}
+          </div>
+        )}
+
         {formType === "signedIn" && (
-          <div>
-            <h1>Hello user!</h1>
-            <Button onClick={() => Auth.signOut()}>Sign Out</Button>
+          <div
+            style={{
+              textAlign: "center",
+              marginTop: "80px",
+            }}
+          >
+            <h1 style={{ fontSize: "30px" }}>Welcome!</h1>
+            <img
+              src={profile}
+              alt="Profile Icon"
+              style={{
+                marginTop: "40px",
+                marginLeft: "auto",
+                marginRight: "auto",
+                display: "block",
+              }}
+            />
+            <h2 style={{ marginTop: "20px", marginBottom: "40px" }}>
+              Get started by building your profile and earn rewards
+            </h2>
+            <Button
+              width="100%"
+              style={{
+                backgroundColor: "#232138",
+                borderRadius: "25px",
+                marginTop: "50px",
+                minHeight: "52px",
+              }}
+              _focus={{ boxShadow: "none" }}
+            >
+              Build My Profile{" "}
+            </Button>
+            <Button
+              width="100%"
+              style={{
+                backgroundColor: "#232138",
+                borderRadius: "25px",
+                marginTop: "50px",
+                minHeight: "52px",
+              }}
+              _focus={{ boxShadow: "none" }}
+              onClick={() => Auth.signOut()}
+            >
+              Sign Out
+            </Button>
           </div>
         )}
       </Container>
